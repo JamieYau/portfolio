40c4749e8d79c66873156f6bc635361d12d9e852
JamieYau <jamie.yau2002@me.com>
2023-09-10 17:36:23 +0100

    Add dynamic offsets for header links

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,43 @@
 import "./style.css";
+
+function setHomePageHeight() {
+  const header = document.querySelector("header") as HTMLHeadElement;
+  const headerHeight = header.offsetHeight;
+  const windowHeight = window.innerHeight;
+  const homePage = document.querySelector("#home") as HTMLElement;
+  homePage.style.height = windowHeight - headerHeight + "px";
+}
+
+function smoothScrollToTarget(targetHref: string) {
+  const header = document.querySelector("header") as HTMLHeadElement;
+  const headerHeight = header.offsetHeight;
+
+  const target = document.querySelector(targetHref);
+
+  if (target) {
+    const targetPosition = target.getBoundingClientRect().top - headerHeight;
+
+    window.scrollTo({
+      top: targetPosition + window.scrollY,
+      behavior: "smooth",
+    });
+  }
+}
+
+// Call setHomePageHeight when the page loads
+window.addEventListener("load", setHomePageHeight);
+
+// Offset the anchor links to account for the header
+const anchorLinks = document.querySelectorAll("a[href^='#']");
+anchorLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    // Cast e.target to HTMLAnchorElement
+    const targetHref = (e.target as HTMLAnchorElement).getAttribute("href");
+
+    if (targetHref) {
+      smoothScrollToTarget(targetHref);
+    }
+  });
+});
